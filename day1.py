5f5c62220381e521f380e2e0e8c9aec70bd25f4a
Shade <55607744+schmeow@users.noreply.github.com>
2023-12-01 16:36:35 -0500

    finished day 1

---
+++ day1.py
@@ -0,0 +1,28 @@
+import re
+import sys
+
+# Part One
+
+""" sum = 0
+for case in range(1000):
+    word = sys.stdin.readline().rstrip()
+    nums = re.findall("\d+", " ".join(word))
+    value = int(nums[0] + nums[len(nums) - 1])
+    sum = sum + value
+
+print(sum) """
+
+sum = 0
+numlist = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+for case in range(1000):
+    word = sys.stdin.readline().rstrip()
+    nums = []
+    for i,letter in enumerate(word):
+        if letter.isdigit():
+            nums.append(letter)
+        for num in numlist:
+            if word[i:].startswith(num):
+                nums.append(str(numlist.index(num)))
+    value = int(nums[0] + nums[len(nums)-1])
+    sum = sum + value
+print(sum)
